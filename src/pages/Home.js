de5315a7539ce8b75b8d6e0f5381bc39a6587844
Gary <developer.garylouw@gmail.com>
2022-05-24 11:49:48 +0200

    Project Section added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
 import HeroSection from "../components/HeroSection";
+import ProjectSection from "../components/ProjectSection";
 import ServicesSection from "../components/ServicesSection";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <HeroSection />
       <AboutSection />
       <ServicesSection />
+      <ProjectSection />
     </div>
   );
 }
